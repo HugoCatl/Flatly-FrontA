e95b3dcdc59330a6105fc984e23a325056f9035a
pca100 <pca1002004@gmail.com>
2026-02-24 18:37:05 +0100

    fix autentification

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://26.173.14.24:8080' // Update this with your production API URL
+  apiUrl: 'http://26.173.14.24:8080'// 'http://localhost:4200'// Update this with your production API URL
 };
