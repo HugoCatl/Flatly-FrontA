a4897b31a6641a7faabeea1e80be43bac73f203c
paulaBenlloch2710 <paulibg06@gmail.com>
2026-02-24 16:56:49 +0100

    modo oscuro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('mi-app-mobile');
+
+  readonly theme = inject(ThemeService);
+
+  ngOnInit() {
+    this.theme.init();
+  }
 }
