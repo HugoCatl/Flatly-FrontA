27e911129b95e702a360a8e1ab5b81784ea24f37
pca100 <pca1002004@gmail.com>
2026-03-02 20:29:08 +0100

    vpn movile

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
   // VPN DOCKER
-  apiUrl: ''//http://localhost:8080
+  //apiUrl: ''//http://localhost:8080
+  // En tu environment.ts o servicio de API
+apiUrl: 'https://occupative-mertie-cushiony.ngrok-free.dev'
 };
